2730f163b8c895a352d8cae5f5fb5258a1300480
Jorge Castilla <jorgecastillagallardo@gmail.com>
2023-12-30 21:21:41 -0300

    Add scrolldown button and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()],
+  integrations: [tailwind(), react(), icon(), vue()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/components/vue/ScrolldownButton.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  left: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const scrollToAbout = () => {
+  const about = document.getElementById('About');
+  about.scrollIntoView({ behavior: 'smooth' });
+};
+
+</script>
+<template>
+  <button
+    class="absolute bottom-24 rotate-90 md:text-lg text-slate-500 scroll-down-animation"
+    :class="left ? '-left-8' : '-right-8'"
+    @click="scrollToAbout"
+  >
+    scroll down
+  </button>
+</template>
+
+<style scoped>
+.scroll-down-animation {
+  animation: 3s linear 0s normal scrollDown;
+}
+
+@keyframes scrollDown {
+  0% {
+    opacity: 0;
+    transform: translateY(-10px) rotate(90deg);
+  }
+  60% {
+    opacity: 0;
+    transform: translateY(-5px) rotate(90deg);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px) rotate(90deg);
+  }
+}
+</style>
